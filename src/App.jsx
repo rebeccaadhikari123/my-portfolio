057b5aa4a3bf94c41f4426326096e5f4b35b62a5
rebeccaadhikari123 <85625859+rebeccaadhikari123@users.noreply.github.com>
2023-03-06 16:36:22 +0545

    created files on src and edited index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import Navbar from './sections/navbar/Navbar'
+import Header from './sections/header/Header';
+import About from './sections/about/About';
+import Portfolio from './sections/portfolio/Portfolio';
+import Contact from './sections/contact/Contact';
+import Footer from './sections/footer/Footer';
+import FloatingNav from './sections/floating-nav/FloatingNav';
+
 
 
 
 const App = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <main>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+      <FloatingNav/>
+      
+
+    </main>
   )
 }
 
---
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -0,0 +1,10 @@
+import './floating-nav.css'
+
+const FloatingNav = () => {
+  return (
+    <section id="floating__nav"></section>
+
+  )
+}
+
+export default FloatingNav

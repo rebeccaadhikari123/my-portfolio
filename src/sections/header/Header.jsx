ccfac98668ed3ab35eb98aa8d63020a8735708b0
rebeccaadhikari123 <85625859+rebeccaadhikari123@users.noreply.github.com>
2023-03-07 16:40:57 +0545

    Navbar section done

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,28 @@
 import './header.css'
+import HeaderImage  from '../../assets/header.jpg'
 
 const Header = () => {
   return (
     <header id="header">
-        <h2>Header</h2>
+       <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="HeaderImage" />
+        </div>
+        <h5>Hello I'm</h5>
+        <h1>Rebicca Adhikari</h1>
+        <p>I am an energetic, ambitious person who has developed mature and responsible approach to any task that I undertake, or a situation that I am presented with. As a Frontend Developer i will give my excellent performance in working with a company to achieve a certain objective on time and excellence</p>
+        <div className="header__cta">
+           <a href="#contact">Let's talk</a>
+           <a href="#portfolio">My Work</a>
+           
+           </div>  
+           <div className="header__socials"  >
+            {
+              
+            }
+            </div>   
+        
+       </div>
     </header>
   )
 }
--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,26 @@
 import './navbar.css'
+import data from './data'
+import Me  from '../../assets/me.jpg'
+import {IoIosColorPalette} from 'react-icons/io'
 
 const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+    <div className="container nav__container">
+      <a href = "index.html " classname='nav__me' >
+        <img src={Me} alt="Me" />
+        </a>
+        
+      <ul className='nav__menu'>
+        {
+        data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+}
+      </ul>
+      <button id='theme__icon'><IoIosColorPalette/></button>
+
+
+    </div>
+    </nav>
   )
 }
 

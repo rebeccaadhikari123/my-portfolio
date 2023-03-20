2ed290d1e2190bf71d6d5ce7a038e576dcc3bd33
rebeccaadhikari123 <85625859+rebeccaadhikari123@users.noreply.github.com>
2023-03-20 21:45:12 +0545

    portfoilo and contacts change

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -2,7 +2,21 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio"><h2>Portfolio</h2></section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="contaner portfolio__container">
+        <article className='portfolio__iteam'>
+          <div className="portfolio__iteam-image"></div>
+          <h3>This is a portfolio iteam title</h3>
+
+          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+
+        </article>
+      </div>
+      </section>
   )
 }
 
